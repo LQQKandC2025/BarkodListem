2248111d2d165dde4cdbd4f4007968fdde8503da
MONSTER <MONSTER@MONSTERPC>
2025-07-09 00:27:35 +0300

    Add IrsaliyeModel class for invoice representation
    Introduces the `IrsaliyeModel` class in the `BarkodListem.Models` namespace. This class includes properties for `IRSALIYE_ID`, `IRSALIYE_TURU`, `IRS_TARIH`, and `IRS_NO` to represent invoice attributes. Necessary using directives for system libraries are also included to support the use of types like `DateTime`.

---
+++ BarkodListem/Models/IrsaliyeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkodListem.Models
+{
+    public class IrsaliyeModel
+    {
+        public string IRSALIYE_ID { get; set; }
+        public string IRSALIYE_TURU { get; set; }
+        public DateTime IRS_TARIH { get; set; }
+        public string IRS_NO { get; set; }
+    }
+}
